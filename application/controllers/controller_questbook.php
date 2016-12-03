dd30fe0e9d7f88b43abf1a752ff44a83196fb066
Timur <timka_issaev@mail.ru>
2016-12-03 03:42:03 +0300

    added AJAX a_add_msg() function to create messages

--- application/controllers/controller_questbook.php
+++ application/controllers/controller_questbook.php
@@ -4,13 +4,20 @@ class Controller_Questbook extends Controller {
   public function __construct() {
     $this->i18n = new i18n;
     $this->view = new View;
+    $this->model = new Model_Questbook;
   }
 
   public function action_index() {
     $i18n = $this->i18n->get(array('questbook'));
     $param['css'] = array('questbook');
     $param['title'] = $i18n['questbook'];
+    $data['messages'] = $this->model->getMessages();
     $this->view->generate('questbook_view.php', 'template_view.php', $param, $data, $i18n);
+  }
+
+  public function action_a_add_msg() {
+    $data['add_msg'] = $this->model->addMsg($_POST['firstname'], $_POST['message']);
+    $this->view->generate(null, '/ajax/questbook/add_msg.php', null, $data, $i18n);
   }
   
 }
---
+++ application/models/model_questbook.php
@@ -0,0 +1,74 @@
+<?php
+class Model_Questbook extends Model {
+
+  const MIN_FIRSTNAME = 3;
+  const MAX_FIRSTNAME = 21;
+
+  const MIN_MESSAGE = 3;
+  const MAX_MESSAGE = 147;
+
+  public $max_rows = 10;
+
+  public $captcha;
+  public $database;
+  public $date;
+  public $i18n;
+
+  
+  public function __construct() {
+    $this->captcha = new Captcha;
+    $this->database = DataBase::connect();
+    $this->date = new Date;
+    $this->i18n = new i18n;  
+  }
+
+  public function addMsg($firstname, $message) {
+    $firstname_length = mb_strlen($firstname);
+    $message_length = mb_strlen($message);
+
+    if($firstname_length < self::MIN_FIRSTNAME) {
+      return array('err'=>true,'err_msg'=>$this->i18n->get('short_firstname'));
+    } elseif($firstname_length > self::MAX_FIRSTNAME) {
+      return array('err'=>true,'err_msg'=>$this->i18n->get('long_firstname'));
+    } elseif(!preg_match('/^[a-zа-яё]{2,110}$/ui', $firstname)) {
+      return array('err'=>true,'err_msg'=>$this->i18n->get('incorrect_firstname'));    
+    }
+
+    if($message_length < self::MIN_MESSAGE) {
+      return array('err'=>true,'err_msg'=>$this->i18n->get('short_msg'));
+    } elseif($message_length > self::MAX_MESSAGE) {
+      return array('err'=>true,'err_msg'=>$this->i18n->get('long_msg'));
+    }
+    
+    $ts = time();
+$sql = "INSERT INTO `questbook`(`id`, `owner_name`, `date_created`, `text`) VALUES
+                               ('',:owner_name,:date_created,:_text)";
+    $add_msg = $this->database->prepare($sql);
+
+    $add_msg->execute(array(':owner_name' => 'sadsadas',
+    	                    ':date_created' => '12321',
+    	                    ':_text' => 'sadsdas'));
+    return array('err'=>false,'msg_id'=>$this->database->lastInsertId(),'firstname'=>$firstname,'date_created'=>$ts,'text'=>$message);
+  }
+
+
+  public function getMessages($offset = 0){
+    $offset = intval($offset);
+    $has_more = false;
+    $get_message = $this->database->prepare("SELECT `id`, `owner_name`, `date_created`, `text` FROM `questbook` ORDER BY `id` DESC LIMIT :offset, :max_rows");
+    $get_message->bindParam(':offset', $offset, PDO::PARAM_INT);
+    $get_message->bindParam(':max_rows', $this->max_rows,  PDO::PARAM_INT);
+    $get_message->execute();
+    $dialog_owner = array();
+    while($row = $get_message->fetch(PDO::FETCH_ASSOC)) {
+      $row['date'] = $this->date->parseTimestamp($row['date_created']);
+      $arr[] = $row;
+    }
+    if(count($arr) > $this->max_rows - 1) {
+      $has_more = true;
+    }
+    $offset = $offset + $this->max_rows;
+    return array('err'=>false,'html'=>$arr,'has_more'=>$has_more,'offset'=>$offset);
+  }
+}
+?>
