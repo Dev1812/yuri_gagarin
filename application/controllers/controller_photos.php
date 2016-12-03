26bf08fb590be479f404d2f95cb5635a3fc1a466
Timur <timka_issaev@mail.ru>
2016-12-03 03:37:38 +0300

    added photo_layer key to the data array - he needs to display photo_layer block in template_view.php

--- application/controllers/controller_photos.php
+++ application/controllers/controller_photos.php
@@ -8,8 +8,10 @@ class Controller_Photos extends Controller {
 
   function action_index() {
     $i18n = $this->i18n->get(array('photos'));
-    $param['css'] = array('photos');
+    $param['css'] = array('photos', 'photo_layer');
+    $param['js'] = array('photo_layer');
     $param['title'] = $i18n['photos'];
+    $data = array('photo_layer' => true);
     $this->view->generate('photos_view.php', 'template_view.php', $param, $data, $i18n);
   }
   
