d2ac735683bbbe77dd412ded9b84078c8f25fff1
Timur <timka_issaev@mail.ru>
2016-12-15 11:33:21 +0400

    added functions to get and adding a messages for ajax

--- application/controllers/controller_questbook.php
+++ application/controllers/controller_questbook.php
@@ -5,19 +5,37 @@ class Controller_Questbook extends Controller {
     $this->i18n = new i18n;
     $this->view = new View;
     $this->model = new Model_Questbook;
+    $this->security = new Security;
+    $this->captcha = new Captcha;
+    $this->security->setSessionName('questbook_hash');
   }
 
   public function action_index() {
     $i18n = $this->i18n->get(array('questbook'));
     $param['css'] = array('questbook');
+    $param['js'] = array('questbook');
     $param['title'] = $i18n['questbook'];
     $data['messages'] = $this->model->getMessages();
+    $data['form_hash'] = $this->security->getCSRFToken();
     $this->view->generate('questbook_view.php', 'template_view.php', $param, $data, $i18n);
   }
 
   public function action_a_add_msg() {
-    $data['add_msg'] = $this->model->addMsg($_POST['firstname'], $_POST['message']);
-    $this->view->generate(null, '/ajax/questbook/add_msg.php', null, $data, $i18n);
+    if(!$this->captcha->checkCode($_POST['captcha'])) {
+      //Incorrect Captha Code
+      $data['ajax'] = array('err'=>true,'err_msg'=>$this->i18n->get('captcha_error'),'js'=>'updateCaptcha("captcha_img");ge("captcha_code").focus()');
+    } elseif(!$this->security->checkCSRFToken($_POST['form_hash'])) {
+      //Incorrect Form Hash
+      $data['ajax'] = array('err'=>true,'err_msg'=>'<div class="form_msg_title">'.$this->i18n->get('unknown_error').'</div>');
+    } else {
+      //All is correct, calling addMsg Method
+      $data['ajax'] = $this->model->addMsg($_POST['firstname'], $_POST['message']);
+    }
+    $this->view->generate(null, 'ajax/questbook/add_msg.php', null, $data, $i18n);
   }
   
+  public function action_a_get_more() {
+    $data['ajax'] = $this->model->getMessages($_GET['offset']);
+    $this->view->generate(null, 'ajax/questbook/get_more.php', null, $data);
+  }
 }
--- application/models/model_questbook.php
+++ application/models/model_questbook.php
@@ -8,15 +8,12 @@ class Model_Questbook extends Model {
   const MAX_MESSAGE = 147;
 
   public $max_rows = 10;
-
   public $captcha;
   public $database;
   public $date;
   public $i18n;
 
-  
   public function __construct() {
-    $this->captcha = new Captcha;
     $this->database = DataBase::connect();
     $this->date = new Date;
     $this->i18n = new i18n;  
@@ -30,7 +27,7 @@ class Model_Questbook extends Model {
       return array('err'=>true,'err_msg'=>$this->i18n->get('short_firstname'));
     } elseif($firstname_length > self::MAX_FIRSTNAME) {
       return array('err'=>true,'err_msg'=>$this->i18n->get('long_firstname'));
-    } elseif(!preg_match('/^[a-zа-яё]{2,110}$/ui', $firstname)) {
+    } elseif(!preg_match('/^[a-zа-яё]{2,}$/ui', $firstname)) {
       return array('err'=>true,'err_msg'=>$this->i18n->get('incorrect_firstname'));    
     }
 
@@ -41,14 +38,11 @@ class Model_Questbook extends Model {
     }
     
     $ts = time();
-$sql = "INSERT INTO `questbook`(`id`, `owner_name`, `date_created`, `text`) VALUES
-                               ('',:owner_name,:date_created,:_text)";
-    $add_msg = $this->database->prepare($sql);
-
-    $add_msg->execute(array(':owner_name' => 'sadsadas',
-    	                    ':date_created' => '12321',
-    	                    ':_text' => 'sadsdas'));
-    return array('err'=>false,'msg_id'=>$this->database->lastInsertId(),'firstname'=>$firstname,'date_created'=>$ts,'text'=>$message);
+    $add_msg = $this->database->prepare("INSERT INTO `questbook`(`id`, `owner_name`, `date_created`, `text`) VALUES('',:owner_name,:date_created,:_text)");
+    $add_msg->execute(array(':owner_name' => $firstname,
+    	                    ':date_created' => $ts,
+    	                    ':_text' => $message));
+    return array('err'=>false,'msg_id'=>$this->database->lastInsertId(),'firstname'=>$firstname,'date_created'=> $this->date->parseTimestamp($ts),'text'=>$message);
   }
 
 
@@ -59,16 +53,18 @@ $sql = "INSERT INTO `questbook`(`id`, `owner_name`, `date_created`, `text`) VALU
     $get_message->bindParam(':offset', $offset, PDO::PARAM_INT);
     $get_message->bindParam(':max_rows', $this->max_rows,  PDO::PARAM_INT);
     $get_message->execute();
-    $dialog_owner = array();
+
     while($row = $get_message->fetch(PDO::FETCH_ASSOC)) {
       $row['date'] = $this->date->parseTimestamp($row['date_created']);
       $arr[] = $row;
     }
-    if(count($arr) > $this->max_rows - 1) {
+
+    if(count($arr) >= $this->max_rows) {
       $has_more = true;
     }
+
     $offset = $offset + $this->max_rows;
-    return array('err'=>false,'html'=>$arr,'has_more'=>$has_more,'offset'=>$offset);
+    return array('err'=>false,'messages'=>$arr,'has_more'=>$has_more,'offset'=>$offset);
   }
 }
 ?>
